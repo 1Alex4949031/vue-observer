239a37a3b0ff215f95103fcfafb8b76cb6323759
Алексей Селезнев <a.seleznev@g.nsu.ru>
2025-06-08 00:11:02 +0700

    Паттерн Observer

---
+++ src/components/ReceiveMessage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import { messageBus } from "../events/messageBus";
+
+
+const msg = ref("");
+
+let unsubscribe: () => void;
+
+onMounted(() => {
+  unsubscribe = messageBus.subscribe((data) => {
+    msg.value = data;
+  });
+});
+
+onUnmounted(() => {
+  unsubscribe();
+});
+</script>
+
+<template>
+  <p v-if="msg">📩 Получено: {{ msg }}</p>
+</template>
+
